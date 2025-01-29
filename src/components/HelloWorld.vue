8475ad6d79b0198129741d57e249d23b19c37a5d
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-01-29 12:23:27 +0100

    Add MWE

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    style="
+      width: 680px;
+      height: 420px;
+      position: relative;
+    "
+    id="polarstern"
+  >
+    <!-- Optional, may use if your page does not have its own <noscript> information -->
+    <noscript>Please use a browser with active JavaScript to use the map client.</noscript>
+  </div>
+  <v-btn @click="loadMap">Laden</v-btn>
+</template>
+
+<script setup>
+import mapClient from '@polar/client-generic'
+function loadMap() {
+  mapClient.createMap({
+    containerId: 'polarstern',
+    //services: 'https://geodienste.hamburg.de/services-internet.json',
+    services: '/services.json',
+    enabledPlugins: [
+      'loading-indicator',
+    ],
+    mapConfiguration: {
+      epsg: "EPSG:25832",
+      layers: [{
+        id: '453',
+        visibility: true,
+        type: 'background',
+      }],
+      stylePath: './node_modules/@polar/client-generic/dist/polar-client.css',
+      loadingIndicator: {
+        displayComponent: true,
+      },
+    },
+  })
+}
+</script>
